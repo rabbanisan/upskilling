47a29c5647ae653f311e87ad561bbb6ce3a9ab05
rabbanisan <bani.ramadhan99@gmail.com>
2023-10-28 09:47:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// const db = require("./app/models");
+
+// db.sequelize.sync();
+
+// routes
+app.get("/test", (req, res) => {
+	res.json({ message: "App started" });
+});
+
+// secured routes
+require("./app/routes/peserta.route")(app);
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
+});
